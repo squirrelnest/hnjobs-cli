48bed65a2f6376675112332c851ce521310be83c
mikka <mikkanthrope@gmail.com>
2017-11-07 12:43:39 -0800

    Define user flow

--- lib/hnjobs_controller.rb
+++ lib/hnjobs_controller.rb
@@ -1,5 +1,22 @@
 class HnjobsController
 
+# User Flow: First-Timer
+# 1. user installs gem
+# 2. user starts app
+# 3. CLI says hi and provides instructions
+# 4. user inputs list command
+# 5. CLI shows error message that there is no list yet, please scrape first
+# 5. user inputs #scrape(url)
+# 5. CLI scrapes url
+# 6. CLI outputs list
+# 7. user inputs number of job posting
+# 8. CLI outputs further details about job posting
+# 9. user returns to list via #list
+# 10. user filters list by #search
+# 11. user inputs number of job posting
+# 12. CLI outputs further details about job posting
+# 13. user exits
+
   def call
     puts "Hello World"
   end
