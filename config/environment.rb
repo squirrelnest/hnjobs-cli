a9d7fa88f2a2e2dee8d9513a3df18ab51a53db37
mikka <mikkanthrope@gmail.com>
2017-11-09 13:06:19 -0800

    Remove dir glob

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,8 @@
 require 'bundler'
 Bundler.require
 
-Dir.glob('lib/**/*.rb') do |file|
-  require_relative File.join('../', file)
-end
+# Dir.glob('lib/**/*.rb') do |file|
+#   require_relative File.join('../', file)
+# end
 
 # autoload_paths += %W(#{config.root}/lib)
